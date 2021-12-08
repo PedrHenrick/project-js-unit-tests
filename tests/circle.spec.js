9bd5c0d15c4d5efa7c8dfc0b2e2749c242590f64
Pedro Henrick <phmc@ic.ufal.br>
2021-12-08 18:19:25 -0300

    Requisitos 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,13 +28,23 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
     expect(circle('4')).toBeUndefined();
+    expect(typeof circle(4)).toBe('object');
     // Teste se circle retorna um objeto.
     expect(typeof circle(4)).toBe('object');
     // Teste se o objeto retornado tem 3 propriedades.
-    expect(Object.keys(circle).length).toBe('3');
+    const total = Object.keys(circle(4)).length;
+    expect(total).toBe(3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle()).toBeUndefined();
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    const circ = circle(2).circumference;
+    expect(circ).toBeCloseTo(12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    const are = circle(3).area;
+    expect(are).toBeCloseTo(28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    const circ2 = circle(3).circumference;
+    expect(circ2).toBeCloseTo(18.84);
+
   });
 });
